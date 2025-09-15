e9741bf1e7ae3f87832796a7ad1817954d3541fe
Klaskerotte <samuelhaugum@gmail.com>
2025-09-15 11:34:44 +0200

    added a playground file for testing

--- test.cpp
+++ test.cpp
@@ -3,7 +3,13 @@
 int main()
 {
 
-    std::cout << "Hello world" << "Hello world" << std::endl;
+    int x = 2;
+    int a = 5;
+
+    if (x < a)
+    {
+        std::cout << "The number X is lower than " << a << std::endl;
+    }
 
     return 0;
 }
