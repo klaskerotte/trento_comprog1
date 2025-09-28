5c0d102dc7ed789d4762be5be19eccba5a1281ae
Klaskerotte <samuelhaugum@gmail.com>
2025-09-28 21:04:41 +0200

    deleted powerpoints

---
+++ lab_exercises/lab_binary_rep/binaryrep.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+cout << sizeof(unsigned int) << endl;
+cout << sizeof(int) << endl;
+cout << sizeof(unsigned short) << endl;
+cout << sizeof(short) << endl;
+cout << sizeof(unsigned long) << endl;
+cout << sizeof(long) << endl;
+
+
+
+return 0;
+}
