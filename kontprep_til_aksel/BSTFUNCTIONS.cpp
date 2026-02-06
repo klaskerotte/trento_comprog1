d30afe59c710d3c7bdf4067c5a4494bb1f1ebe2b
Klaskerotte <samuelhaugum@gmail.com>
2026-02-06 22:02:06 +0100

    .

--- kontprep_til_aksel/BSTFUNCTIONS.cpp
+++ kontprep_til_aksel/BSTFUNCTIONS.cpp
@@ -3,17 +3,25 @@
 #include <iostream>
 
 int *createarray(int size) {
+
+  // Sett av nok minne til å holde alle integersa dine. Se på dette som "size"
+  // antall tomme prikker i lufta.
   int *array = new int[size];
 
+  // Sett in et random tall i hver index av arrayen
   for (int i = 0; i < size; i++)
     array[i] = std::rand() % (10 + 1);
 
+  // Spytt ut den lagde arrayen din
   return array;
 }
 
 void printarray(int *array, int size) {
 
+  // Lag en tekststreng
   std::string out = "[";
+
+  // For hvert element i arrayen sett inn tallet i strengen din
   for (int i = 0; i < size; i++) {
     out += std::to_string(array[i]);
     if (i != size - 1)
@@ -26,37 +34,54 @@ void printarray(int *array, int size) {
 
 node *insert_element(node *tree, int value) {
 
+  // Hvis du ikke peker på en node, lag en med verdien av interesse
   if (!tree)
     return new node{value, nullptr, nullptr};
 
+  // Hvis verdien er større/mindre enn nodeverdien du ser på, gå til
+  // høyre/venstre og prøv der.
   if (value > tree->data)
     tree->right = insert_element(tree->right, value);
   else
     tree->left = insert_element(tree->left, value);
 
+  // Spytt ut treet du har laget med den innsatte verdien
   return tree;
 }
 
 node *createBST(int *array, int size) {
+  // Hvis arrayen din ikke eksisterer returner nada niks null
   if (size == 0)
     return nullptr;
 
+  // Lag første node med første verdien i arrayen
   node *tree = new node{array[0], nullptr, nullptr};
 
+  // For hvert element i arrayen din, sammenlign med treet du har bygd og sett
+  // inn på riktig plass. Se "insert_element" funksjonen over
   for (int i = 1; i < size; i++) {
     tree = insert_element(tree, array[i]);
   }
 
+  // Returner det lagde treet
   return tree;
 }
 
 void fancyprintBST(node *tree, int depth = 0) {
+  // Hvis du peker på ingenting returner ingenting og avslutt funksjonen
   if (!tree)
     return;
+
+  // Start en rekursivitet hvor du går til høyre frem til du når slutten og
+  // print ut
   fancyprintBST(tree->right, depth + 1);
   for (int i = 0; i < depth; i++) {
     std::cout << "   ";
   }
+
+  // Print ut verdien av interesse
   std::cout << tree->data << std::endl;
+
+  // Start en rekursivitet hvor du går til venstre frem til du når slutten.
   fancyprintBST(tree->left, depth + 1);
 }
--- kontprep_til_aksel/BSTFUNCTIONS.h
+++ kontprep_til_aksel/BSTFUNCTIONS.h
@@ -1,11 +1,19 @@
-#ifndef BSTFUNCTIONS
-#define BSTFUNCTIONS
+#ifndef BSTFUNCTIONS // Dette sier at filen ikke skal inkluderes mer enn én gang
+#define BSTFUNCTIONS // Dette også
 
-// Dette er en headerfil
-// Tenk på dette som en enkel hånd for funksjonene. De sier
+// Dette er en headerfil. Derav .h på slutten av filen
+// Tenk på dette som en enkel håndbok for funksjonene. De sier
 //      hvilken type funksjonen returnerer
 //      hva funksjonen heter
+//      hvilke argumenter funksjonen tar inn
 //
+// For å se hvordan selve funksjonene fungerer, sjekk BSTFUNCTIONS.cpp filen
+
+// Definerer en type objekt som heter node.
+// Hver node inneholder:
+//      data som er en integer
+//      left som er en pointer til venstre node
+//      right som er en pointer til høyre node
 
 struct node {
   int data;
@@ -13,10 +21,21 @@ struct node {
   node *right;
 };
 
-int *createarray(int size);
-void printarray(int *array, int size);
-node *insert_element(node *tree, int value);
-node *createBST(int *array, int size);
-void fancyprintBST(node *tree, int depth);
+int *createarray(
+    int size); // Lager en array med random integers mellom 0 til 10. Tar inn
+               // størrelsen på arrayen du vil lage.
+
+void printarray(int *array,
+                int size); // Printer arrays på en fin måte. Tar inn en
+                           // arraypointer og størrelsen på arrayen
+
+node *insert_element(node *tree, int value); // En hjelperfunksjon for å sette
+                                             // inn verdier i et binært søketre
+
+node *createBST(int *array,
+                int size); // Tar inn en array av integers og arrangerer disse i
+                           // en binær søketre struktur.
+
+void fancyprintBST(node *tree, int depth); // Printer ut tréet ditt
 
 #endif // BSTFUNCTIONS
